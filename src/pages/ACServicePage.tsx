e7d72b5129a51a8aef14a0fcf248e4b0980e131b
silica003 <ahammadshammas080@gmail.com>
2026-06-08 18:40:32 +0530

    Updated Freeze Control theme

--- src/pages/ACServicePage.tsx
+++ src/pages/ACServicePage.tsx
@@ -147,8 +147,10 @@ const ACServicePage = () => (
               <div className="mt-4 flex items-center text-xs font-bold text-accent uppercase tracking-wider opacity-0 group-hover:opacity-100 transition-opacity">
                 Professional Service Available
               </div>
-              <div><Button className="mt-4 flex items-center text-xs font-bold text-accent uppercase tracking-wider opacity-0 group-hover:opacity-100 transition-opacity"> 
-                Professional Service Available
+              <div>
+                <Link to="/contact" className="w-full"></Link>
+                <Button className="mt-4 flex items-center text-xs font-bold text-accent uppercase tracking-wider opacity-0 group-hover:opacity-100 transition-opacity"> 
+                Buy a service
                 </Button>
               </div>
               
